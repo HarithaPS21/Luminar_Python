47a915a550e2284b3caa453b3db03d90dfe5dc88
Haritha P S <harithaps59@gmail.com>
2021-07-16 09:21:46 +0530

    added a new file book_sold

--- Advanced_python/object_oriented_programming/book_sold_sorted.py
+++ Advanced_python/object_oriented_programming/book_sold_sorted.py
@@ -1,1 +1,19 @@
-books={
+
+class Books:
+    books={
+    "alchemist":{"Book_name":"alchemist","author":"paulo","price":200,"av_copies":100,"sold":10},
+    "halfgirlfriend":{"Book_name":"hfg","author":"chethan","price":300,"av_copies":300,"sold":200},
+    "rainraising":{"Book_name":"rainraising","author":"nirupama","price":350,"av_copies":10,"sold":340}
+
+    }
+    def findBook(self,Book_name):
+        self.Book_name=Book_name
+        for book in books:
+             print(self.books[book]["Book_name"],self.books[book]["av_copies"])
+obj=Books()
+obj.findBook("alchemist")
+
+
+# res=sorted(books.items(),key=lambda x:x[0]["sold"],reverse=True)
+# for val in res:
+#     print(val[1]["author"])
